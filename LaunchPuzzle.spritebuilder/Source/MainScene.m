66c8ccfb1f1d50c2c95252e1aaa3f19e516273b7
Yizhe Chen <yzchen1991@gmail.com>
2015-03-17 19:18:09 -0400

    add start scene and transition to main scene

--- LaunchPuzzle.spritebuilder/Source/MainScene.m
+++ LaunchPuzzle.spritebuilder/Source/MainScene.m
@@ -1,5 +1,11 @@
 #import "MainScene.h"
+#import "GameScene.h"
 
 @implementation MainScene
+
+-(void)play {
+    GameScene *gameScene = [CCBReader loadAsScene:@"GameScene"];
+    [[CCDirector sharedDirector] replaceScene:gameScene];
+}
 
 @end
