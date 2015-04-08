b5dd257e723306a5ae79ced2cf996b5c567ede0a
Yizhe Chen <yzchen1991@gmail.com>
2015-04-08 15:24:22 -0400

    update spritebuilder version

---
+++ LaunchPuzzle.spritebuilder/PlateTool.h
@@ -0,0 +1,10 @@
+//
+// Created by Yizhe Chen on 4/8/15.
+// Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PlateTool : NSObject
+@end
---
+++ LaunchPuzzle.spritebuilder/PlateTool.m
@@ -0,0 +1,12 @@
+//
+// Created by Yizhe Chen on 4/8/15.
+// Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "PlateTool.h"
+
+
+@implementation PlateTool {
+
+}
+@end
